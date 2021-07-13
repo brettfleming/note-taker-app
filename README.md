870a354764fd88414f1fa27c476ac9976f73c04e
Brett Fleming <brettfleming015@gmail.com>
2021-07-13 17:45:35 -0500

    got the server js working saving and loading the notes

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # note-taker-app
+
+
+![Example webpage image]()
+
+## Task
+
+
+## Table of Contents
+- [Task](#task)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+
+## Description
+
+
+
+## Installation
+you will need to run npm init and npm install for this prject to work. 
+
+
+## Usage
+ 
+
+
+## Contributing
+The only one to contribute to this project so far is me.
+
+
+## Tests
+no tests at the moment
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const savedNotes = require("./db/db.json");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => res.json(savedNotes));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    savedNotes.push(req.body);
+    // console.log(savedNotes);
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
